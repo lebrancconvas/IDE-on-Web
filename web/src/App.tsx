6f8d1994a4e889bffbf655806c6d4c05a23bc8cc
lebrancconvas <lebrancconvas@gmail.com>
2022-06-09 14:11:49 +0700

    💄 Increase Height of Editor.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,7 +4,7 @@ import Editor from '@monaco-editor/react';
 function App() {
   return (
     <div className="App">
-      <Editor height="100vm" theme="vs-dark" defaultLanguage="typescript" /> 
+      <Editor height="776px" theme="vs-dark" defaultLanguage="typescript" /> 
     </div>
   );
 }
